f7a83d11be9700ef4c552ac917c0bf5c5c1af145
Ivana Mihalek <ivana.mihalek@gmail.com>
2020-05-06 14:45:35 +0200

    sqlite version

--- sqlite/16_drug_mechanism_per_variant.py
+++ sqlite/16_drug_mechanism_per_variant.py
@@ -16,9 +16,9 @@ ignored = {"kd", "phenobarbitone", "phenobarbital", "biotin", "pyridoxine", "imm
 #                 'SIGMAR1', "NQO2", 'TAAR1', 'SV2A', 'GABBR', 'CYP', 'ABAT', 'SCN']
 
 
-#########################################
+##################################################################################
 def gnao1_connect():
-	db = sqlite3.connect('/storage/bkps/gnao1db/gnao1.2May20.sql3.db')
+	db = sqlite3.connect('../data/gnao1.2May20.sql3.db')
 	return db, db.cursor()
 
 
@@ -26,8 +26,7 @@ def search_db(cursor, qry):
 	return list(cursor.execute(qry))
 
 
-
-#########################################
+##################################################################################
 def get_drug_effectivenes(cursor):
 	variants_sorted = []
 	drug_eff_per_variant = {}
@@ -86,7 +85,8 @@ def drugs_in_fabula(cursor):
 
 	return all_drugs, active_moiety
 
-###########################################
+
+################
 def find_targets(cursor, drug, generic_names, targets, drugbank_id):
 	gnms = []
 	# try the name first
@@ -141,9 +141,116 @@ def drugs_decompose(cursor, drugs):
 			exit()
 			continue
 
-	return [generic_names, drugbank_id, targets]
+	return [generic_names, targets]
 
-#########################################
+
+################
+def get_direction(action):
+	if not action: return "unk"
+	if action in ["agonist", "partial agonist", "positive allosteric modulator", "potentiator", "activator", "positive"]:
+		return "up"
+	elif action in ["antagonist", "inverse agonist", "blocker", "inhibitor", "negative"]:
+		return "down"
+	else:
+		return "unk"  # not recognized (such as "ligand", "binder" etc)
+
+
+################
+def get_activities(cursor, drugs, generic_names, targets, active_moiety):
+	not_found = 0
+	found = 0
+	target_activity = {}
+	for drug in sorted(drugs):
+
+
+		target_activity[drug] = {}
+
+		for generic_name in generic_names[drug]:
+			active = active_moiety.get(generic_name, generic_name)
+			if not targets[active]:
+				not_found += 1
+				continue
+
+			for tgt in targets[active]:
+				if ":" not in tgt: tgt += ":unk"
+				hgnc_id, action = tgt.split(":")
+
+				direction = get_direction(action.lower())
+				target_activity[drug][hgnc_id.upper()] = [direction, 10000000]
+
+			for table in ["bindingdb_ki", "pdsp_ki", "pubchem_ki", "literature_ki", "guidetopharm_ki"]:
+				qry = "select target_symbol, ki_nM, mode_of_action from {} where drug_name='{}'".format(table, active)
+				ret = search_db(cursor, qry)
+				if ret:
+					for line in ret:
+						[hgnc_id, ki, mode_of_action] = line
+						mode_of_action = get_direction(mode_of_action)
+						if hgnc_id not in target_activity[drug]:
+							target_activity[drug][hgnc_id] = [mode_of_action, ki if ki else 10000000]
+						else:
+							if mode_of_action and mode_of_action!="unk":
+								target_activity[drug][hgnc_id][0] = mode_of_action
+
+							if ki and (not target_activity[drug][hgnc_id][1] or target_activity[drug][hgnc_id][1] > ki):
+								target_activity[drug][hgnc_id][1] = ki
+
+		found += 1
+
+	return target_activity
+
+
+################
+main_families = ["GABR", "GABBR", "ADRA", "ADRB", "CHRN", "CHRM", "SLC", "KCN", "MTNR", "OPR", "PPAR", "PCC", "MCC",  "CYP",
+				  "GRI", "SCN",  "CACNA", "CA",   "MAO",  "DRD",  "HTR", "FCGR", "C1Q", "ITG", "HDAC", "HRH", "NR3C", "PTGER"]
+direction_symbol = {"up":"↑", "down":"↓", "unk":"?"}
+direction_symbol_rotated = {"up":"→", "down":"←", "unk":"?"}
+
+def collapse_coarse(targets):
+
+	compact = []
+
+	group = {}
+	for target, activity in targets.items():
+		family_found = False
+		for family in main_families:
+			target = target.replace("GABAA","GABR")
+			if target[:len(family)] == family:
+				if not family in group:
+					group[family] = {'directions': set(), 'kis': []}
+				group[family]['directions'].add(activity[0])
+				group[family]['kis'].append(activity[1])
+				family_found = True
+				break
+		if not family_found:
+			group[target] = {'directions': {activity[0]}, 'kis': [activity[1]]}
+
+	sorted_families = sorted(group.keys(), key = lambda family: min(group[family]['kis']))
+	cutoff_ki = 100*min(group[sorted_families[0]]['kis'])
+	for family in sorted_families:
+		activities = group[family]
+		min_ki = int(min(activities['kis']))
+		if min_ki>cutoff_ki: continue
+
+		direction = 'unk'
+		if {'down','up'}.issubset(activities['directions']):
+			direction = 'unk'
+		elif 'down' in activities['directions']:
+			direction = 'down'
+		elif 'up' in activities['directions']:
+			direction = 'up'
+		compact.append([family, direction, min_ki])
+	return compact
+
+
+################
+def make_compact_profiles(target_activity):
+	compact_profile = {}
+	for drug, targets in target_activity.items():
+		compact_profile[drug] = collapse_coarse(targets)
+	return compact_profile
+
+
+################
 def target_representative(target):
 	# for mf in main_families:
 	# 	if not mf in target: continue
@@ -151,7 +258,7 @@ def target_representative(target):
 	return target
 
 
-#########################################
+######################################################################################################
 def sort_out_weights_per_variant(variant, effectiveness, targets_compact, weight, split_direction=True):
 	# effecitveness shoud be "eff" or "ineff"; perhaps I should have a way to enforce is
 
@@ -184,8 +291,6 @@ def sort_out_weights_per_variant(variant, effectiveness, targets_compact, weight
 		target_key = f"{target} {direction_symbol[direction]}" if split_direction else target
 		if target_key not in weight[variant]: weight[variant][target_key] = 0
 		weight_term = max(-math.log10(ki)+6, 0) # 0 is bad enough
-		if "SLC" in target_key:
-			print(target_key, ki, weight_term)
 		if split_direction:
 			if effectiveness == "eff":
 				weight[variant][target_key] += weight_term
@@ -227,8 +332,6 @@ def drug_effectivness_matrix(cursor, targets_compact):
 		if len( weight_per_position[position]) == 0: continue
 		twoD_matrix[position] = {}
 		targets = weight_per_position[position].keys()
-		print("***********************************")
-		print(position, weight_per_position[position] )
 		maxval = max([abs(v) for v in weight_per_position[position].values()])
 
 		for target in targets:
@@ -244,53 +347,38 @@ def main():
 	db, cursor = gnao1_connect()
 
 	all_drugs, active_moiety = drugs_in_fabula(cursor)
-	[generic_names, drugbank_id, targets] = drugs_decompose(cursor, list(all_drugs) + list(active_moiety.values()))
-	print(generic_names, drugbank_id, targets)
-	#
-	# target_activity = get_activities(cursor, all_drugs, generic_names, drugbank_id, targets, active_moiety)
-	# targets_compact = make_compact_profiles(target_activity)
-	# [twoD_matrix, targets, aa] = drug_effectivness_matrix(cursor, targets_compact)
+	[generic_names, targets] = drugs_decompose(cursor, list(all_drugs) + list(active_moiety.values()))
+	target_activity = get_activities(cursor, all_drugs, generic_names, targets, active_moiety)
+	targets_compact = make_compact_profiles(target_activity)
+
+	[twoD_matrix, targets, aa] = drug_effectivness_matrix(cursor, targets_compact)
 
 	cursor.close()
 	db.close()
 
-	# positions = twoD_matrix.keys()
-	# print(len(positions), len(targets))
-	#
-	# print("%12s"%"", end="")
-	# for position in positions:
-	# 	print("%7d"%position, end="")
-	# print()
-	# for target in targets:
-	#
-	# 	print("%12s"%target[:10], end="")
-	# 	for position in positions:
-	# 		print("%7.2f"%twoD_matrix[position].get(target,50), end="")
-	# 	print()
-	#
-	# positions = list(filter(lambda position: len(twoD_matrix[position])>0
-	#                                     and max([abs(d) for d in twoD_matrix[position].values()])>0.01, positions))
-	# data = {}
-	# for target in targets:
-	# 	# if target in weak_targets: continue
-	# 	auxlist = []
-	# 	for position in positions:
-	# 		auxlist.append(twoD_matrix[position].get(target,0))
-	# 	if len(auxlist) == 0: continue
-	# 	if max([abs(d) for d in auxlist])<0.1: continue
-	# 	data[target] = auxlist
-	# # https://note.nkmk.me/en/python-pandas-dataframe-rename
-	# named_positions = [f"{aa[p]}{p}" for p in positions]
-	# pandas_data_frame = pd.DataFrame(data, index=named_positions).transpose()
-	# # matplotlib colormaps: https://matplotlib.org/tutorials/colors/colormaps.html
-	# # cbar turns off the colorbar
-	# #ax = sns.heatmap(pandas_data_frame, center = 0, cmap="seismic", cbar=False)
-	# sns.set(font_scale=1.5)
-	# ax = sns.clustermap(pandas_data_frame, center=0, cmap="seismic")
-	# plt.setp(ax.ax_heatmap.yaxis.get_majorticklabels(), rotation=0) # ylabels rotatesd otherwise
-	#
-	# #plt.show() # beats me how this knows what to plot, but id does
-	# plt.savefig("gnao_clustermap.svg")
+	positions = twoD_matrix.keys()
+	positions = list(filter(lambda position: len(twoD_matrix[position])>0
+	                                    and max([abs(d) for d in twoD_matrix[position].values()])>0.01, positions))
+	data = {}
+	for target in targets:
+		# if target in weak_targets: continue
+		auxlist = []
+		for position in positions:
+			auxlist.append(twoD_matrix[position].get(target,0))
+		if len(auxlist) == 0: continue
+		if max([abs(d) for d in auxlist])<0.1: continue
+		data[target] = auxlist
+
+	# https://note.nkmk.me/en/python-pandas-dataframe-rename
+	named_positions = [f"{aa[p]}{p}" for p in positions]
+	pandas_data_frame = pd.DataFrame(data, index=named_positions).transpose()
+	# matplotlib colormaps: https://matplotlib.org/tutorials/colors/colormaps.html
+	# cbar turns off the colorbar
+	sns.set(font_scale=1.5)
+	ax = sns.clustermap(pandas_data_frame, center=0, cmap="seismic")
+	plt.setp(ax.ax_heatmap.yaxis.get_majorticklabels(), rotation=0) # ylabels rotatesd otherwise
+
+	plt.savefig("gnao_clustermap.png")
 
 #########################################
 if __name__ == '__main__':
