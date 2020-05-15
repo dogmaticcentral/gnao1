b33fcbe518e73a8bc490b5750a0f450b4599dc88
Ivana Mihalek <ivana.mihalek@gmail.com>
2020-05-15 11:36:46 +0200

    mouse

--- 40_allen.py
+++ 40_allen.py
@@ -12,11 +12,11 @@ def download(gene, fnm):
 	print(fnm)
 
 
-def human_brain_data(fnm):
+def brain_data(fnm, species):
 	expression = {}
 	with open(fnm) as inf:
 		for line in inf:
-			if not'Allen Brain Atlas Adult Human Brain Tissue Gene Expression Profiles' in line: continue
+			if not f"Allen Brain Atlas Adult {species.capitalize()} Brain Tissue Gene Expression Profiles" in line: continue
 			[location, dataset,	threshold_value, standardized_value] = line.strip().split("\t")
 			expression[location] = "%.2f"%float(standardized_value)
 	return expression
@@ -24,10 +24,12 @@ def human_brain_data(fnm):
 
 def main():
 
+	species = "mouse"
+
 	adcys = [f"adcy{n+1}" for n in range(10)]
 
-	gene_fnm = {"gnao1" : "/storage/databases/allen/gnao1_expression_associations.txt"}
-	for adcy in adcys: gene_fnm[adcy] = f"/storage/databases/allen/{adcy}_expression_associations.txt"
+	gene_fnm = {"gnao1" : f"/storage/databases/allen/{species}/gnao1_expression_associations.txt"}
+	for adcy in adcys: gene_fnm[adcy] = f"/storage/databases/allen/{species}/{adcy}_expression_associations.txt"
 
 	for gene, fnm in gene_fnm.items():
 		if os.path.exists(fnm): continue
@@ -38,19 +40,27 @@ def main():
 
 	expression = {}
 	for gene, fnm in gene_fnm.items():
-		expression[gene] = human_brain_data(fnm)
+		expression[gene] = brain_data(fnm, species)
 
 	locations_sorted = sorted(expression["gnao1"].keys(), key= lambda location: float(expression["gnao1"][location]), reverse=True)
 
-	# revmoe adcys with no positive correlation in expression with adcy5
+	# remove adcys with no positive correlation in expression with adcy5
 	# adcy10 actually has no info at at all
 	for adcy in ["adcy2", "adcy10"]:
 		adcys.remove(adcy)
 
 	print("\t".join(["location", "GNAO1"] + [a.upper() for a in adcys]))
+	# mose has things like STR (striatum) STRd (striatum dorsal) CP *caudoputamen" STRv (striatum ventral), LSX lateravl ventral complex
+	# PAL pallidum
 	for location in locations_sorted:
-		adcy_expression = [expression[gene].get(location,"") for gene in adcys]
-		print( "\t".join([location, expression["gnao1"][location]] + adcy_expression) )
+		for basal_ganglia_kwd in ["striatum", "caudate", "putamen", "pallidus", "caudate", "nigra",
+		                          "accumbens", "subthalamic", "STR", "Str", "PAL", "Caud", "caud", "striatal"]:
+			if not basal_ganglia_kwd in location: continue
+			adcy_expression = [expression[gene].get(location,"") for gene in adcys]
+			print( "\t".join([location, expression["gnao1"][location]] + adcy_expression) )
+	# for location in locations_sorted:
+	# 	adcy_expression = [expression[gene].get(location,"") for gene in adcys]
+	# 	print( "\t".join([location, expression["gnao1"][location]] + adcy_expression) )
 
 	return
 
